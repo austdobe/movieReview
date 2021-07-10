b2a7cbea45c3989892bebc7d7643ff3bdb596557
Austin <austin.dober@gmail.com>
2021-07-10 09:51:20 -0400

    adding API pull to grab images of the 20 movies pulled from call

--- movie-review/src/components/Home.js
+++ movie-review/src/components/Home.js
@@ -6,7 +6,8 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 //components
 import HeroImage from "./HeroImage";
-
+import Grid from "./Grid";
+import Thumb from "./Thumb"
 
 
 //Hook
@@ -14,6 +15,7 @@ import { useHomeFetch } from '../hooks/useHomeFetch'
 
 //Image
 import NoImage from '../images/no_image.jpg'
+
 
 const Home = ( ) => {
 
@@ -25,13 +27,27 @@ const Home = ( ) => {
 
     return (
         <>  
-            {state.results[0] ?( 
+            {state.results[0] ? (
                 <HeroImage 
                     image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                     title={state.results[0].original_title}
                     text={state.results[0].overview}
                 />
-            ): null}   
+            ) : null}  
+            <Grid header= 'Popular Movies'>
+                {state.results.map(movie => (
+                    <Thumb
+                        key={movie.id}
+                        clickable
+                        image={
+                            movie.poster_path
+                            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                            : NoImage
+                        }
+                        movieId={movie.id}
+                    />
+                ))}
+            </Grid>
         </>
     )
 }
